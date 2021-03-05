67869d62fb92a9543e5563fa9c90f25cb5eb5cda
Johnny Keple <Johnny.Keple@gmail.com>
2021-03-05 12:21:25 -0600

    Stretch Goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -58,7 +58,24 @@ if (pets < allowedPets) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
+let mostPets;
 
+if (pets > friendsPets) {
+  mostPets = pets;
+} else {
+  mostPets = friendsPets;
+  console.log(mostPets);
+}
 // 17 - Rewrite question 16 with a `switch` statement. You'll need to do research!
+switch (mostPets) {
+  case pets > friendsPets:
 
+    break;
+  case pets < friendsPets:
+
+    break;
+  default:
+  console.log(pets);
+  break;
+}
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
