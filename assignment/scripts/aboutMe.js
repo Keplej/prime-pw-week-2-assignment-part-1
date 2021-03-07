602aa24e8e3c115ce4cebe4f305d284bd10b0003
Johnny Keple <Johnny.Keple@gmail.com>
2021-03-07 04:34:52 -0600

    Finalversion fixed

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -29,9 +29,9 @@ const allowedPets = 4;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
 if (adventurous) {
-  console.log("Adventures are great!");
+  console.log('Adventures are great!');
 } else {
-  console.log("How about we stay home?");
+  console.log('How about we stay home?');
 }
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
@@ -45,11 +45,11 @@ if (luckyNumber === 2 && adventurous === true) {
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
 if (pets < allowedPets) {
-  console.log("I can have more pets!");
+  console.log('I can have more pets!');
 } else if (pets === allowedPets) {
   console.log('I have enough pets');
 } else if (pets > allowedPets) {
-  console.log("Oh no, I have too many pets!");
+  console.log('Oh no, I have too many pets!');
 }
 // STRETCH GOALS:
 
@@ -79,4 +79,4 @@ switch (mostPets) {
   break;
 }
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-adventurous ? console.log("Adventures are great!") : console.log("How about we stay home?");
+adventurous ? console.log('Adventures are great!') : console.log('How about we stay home?');
